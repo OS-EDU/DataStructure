7b38741c271a911e9edac233d92d6beba4331c90
wangfuzheng <1677980708@qq.com>
2020-05-31 09:25:16 +0800

    增加添加结点方法和显示链表信息方法的注释

--- src/com/quake/linkedlist/DoubleLinkedListDemo.java
+++ src/com/quake/linkedlist/DoubleLinkedListDemo.java
@@ -25,45 +25,55 @@ public class DoubleLinkedListDemo {
 
 // 定义一个类管理结点
 class DoubleLinkedList {
-    private HeroNode2 head = new HeroNode2(0,"","");
+    // 先初始化一个结点，头结点不要动，不存放具体的数据
+    private HeroNode2 head = new HeroNode2(0, "", "");
 
-    // 添加数据
+    // 添加一个结点到双向链表的最后
     public void add(HeroNode2 heroNode2) {
+        // 因为head结点不能动，因此我们需要添加一个辅助变量temp
         HeroNode2 temp = head;
+        // 遍历链表，知=找到链表的最后
         while (true) {
             if (temp.next == null) {// 遍历到链表的最后
                 break;
             }
+            // 如果没有找到将temp后移
             temp = temp.next;// 后移
         }
+        // 当退相互while循环时，temp就指向了链表的最后
+        // 形成了一个双向链表
         temp.next = heroNode2;
         heroNode2.pre = temp;
     }
 
     // 遍历打印输出链表信息
     public void show() {
+        // 先判断链表是否为空
         if (head.next == null) {
             System.out.println("链表为空~");
             return;
         }
+        // 因为头结点不能动，定义一个辅助变量用于遍历
         HeroNode2 temp = head.next;
         while (true) {
+            // 判断是否到链表最后
             if (temp == null) {
                 break;
             }
+            // 输出结点信息
             System.out.println(temp);
-            temp = temp.next;
+            temp = temp.next;// 将temp后移！！！
         }
     }
 }
 
-// 定义一个双向链表结点
+// 定义HeroNode2，每个HeroNode2对象就是一个对象
 class HeroNode2 {
     public int no;
     public String name;
     public String nickname;
-    public HeroNode2 pre;
-    public HeroNode2 next;
+    public HeroNode2 pre;// 指向下一个结点，默认为null
+    public HeroNode2 next;// 指向前一个结点，默认为null
 
     public HeroNode2(int no, String name, String nickname) {
         this.no = no;
@@ -71,6 +81,7 @@ class HeroNode2 {
         this.nickname = nickname;
     }
 
+    // 为了显示方法，重写toString
     @Override
     public String toString() {
         return "HeroNode2{" +
