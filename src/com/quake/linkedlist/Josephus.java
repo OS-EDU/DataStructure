8b1f9a37cd0fd2521fe7acdc570b5a5b426cfdee
wangfuzheng <1677980708@qq.com>
2020-06-07 11:13:47 +0800

    添加“出圈”方法到环形链表

--- src/com/quake/linkedlist/Josephus.java
+++ src/com/quake/linkedlist/Josephus.java
@@ -9,6 +9,7 @@ public class Josephus {
         CircleSingleLinkedList circleSingleLinkedList = new CircleSingleLinkedList();
         circleSingleLinkedList.addBoy(5);
         circleSingleLinkedList.showBoy();
+        circleSingleLinkedList.countNum(1, 2, 5);
     }
 }
 
@@ -51,12 +52,58 @@ class CircleSingleLinkedList {
         // 因为first不能动，因此需要一个辅助指针完成遍历
         Boy curBoy = first;
         while (true) {
-            System.out.printf("小孩的编号%d\n",curBoy.getNo());
+            System.out.printf("小孩的编号%d\n", curBoy.getNo());
             if (curBoy.getNext() == first) {// 说明已经遍历完毕
                 break;
             }
             curBoy = curBoy.getNext();// curBoy后移
         }
+    }
+
+    /**
+     * 根据用户的输入，计算出小孩出圈的顺序
+     *
+     * @param startNo  表示从第几个小孩开始数数
+     * @param countNum 表示数几下
+     * @param nums     表示最初有多少个小孩在圈中
+     */
+    public void countNum(int startNo, int countNum, int nums) {
+        // 先对数据进行校验
+        if (first == null || startNo < 1 || startNo > nums) {
+            System.out.println("参数输入有误，请重新输入~");
+            return;
+        }
+        // 创建一个辅助指针，帮组完成小孩出圈
+        Boy helper = first;
+        //需要创建一个辅助指针（变量）helper，事先应该指向环形链表的最后这个节点
+        while (true) {
+            if (helper.getNext() == first) {// 说明helper指向最后小孩结点
+                break;
+            }
+            helper = helper.getNext();
+        }
+        // 当小孩报数前，先让first和helper指针同时移动 startNo - 1 次，到达需要报数的位置
+        for (int j = 0; j < startNo - 1; j++) {
+            first = first.getNext();
+            helper = helper.getNext();
+        }
+        // 当小孩报数时，让first和helper指针同时移动countNum - 1次，然后出圈
+        // 这里使用循环操作，直到圈中只有一个结点
+        while (true) {
+            if (helper == first) {// 说明圈中只有一个结点
+                break;
+            }
+            // 让first和helper指针同时移动countNum - 1次
+            for (int j = 0; j < countNum - 1; j++) {
+                first = first.getNext();
+                helper = helper.getNext();
+            }
+            // 这时first指向的结点，就是要出圈的小孩结点
+            System.out.printf("小孩%d出圈\n",first.getNo());
+            first = first.getNext();
+            helper.setNext(first);
+        }
+        System.out.printf("最后留在圈中的小孩编号%d\n",first.getNo());
     }
 }
 
