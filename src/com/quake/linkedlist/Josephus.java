3c6e729770b77ed9184be6183e928dca9b56598f
wangfuzheng <1677980708@qq.com>
2020-06-07 10:38:57 +0800

    添加结点到环形链表

--- src/com/quake/linkedlist/Josephus.java
+++ src/com/quake/linkedlist/Josephus.java
@@ -9,8 +9,32 @@ public class Josephus {
 
 // 创建一个环形的单向链表
 class CircleSingleLinkedList {
+    // 创建一个first结点，当前没有编号
     private Boy first = null;
 
+    // 添加小孩结点，构建一个环形的链表，nums表示需要添加结点的个数
+    public void addBoy(int nums) {
+        if (nums < 1) {//检验
+            System.out.println("nums的值不正确");
+            return;
+        }
+        Boy curBoy = null;// 辅助指针，帮助构建环形链表
+        // 使用for循环俩创建环形链表
+        for (int i = 1; i < nums; i++) {
+            // 根据编号，创建小孩结点
+            Boy boy = new Boy(i);
+            // 如果是第一个小孩
+            if (i == 1) {
+                first = boy;
+                first.setNext(first);// 构成环
+                curBoy = first;
+            } else {
+                curBoy.setNext(boy);
+                boy.setNext(first);
+                curBoy = boy;
+            }
+        }
+    }
 }
 
 // 创建一个Boy类，表示一个结点
