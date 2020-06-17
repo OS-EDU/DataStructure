e7c62c1fe096259b1997051611bc1a3fe39e70d0
wangfuzheng <1677980708@qq.com>
2020-06-17 14:16:50 +0800

    创建相关变量以及构造器

--- src/com/quake/stack/ArrayStackDemo.java
+++ src/com/quake/stack/ArrayStackDemo.java
@@ -9,5 +9,14 @@ public class ArrayStackDemo {
 
 // 定义一个ArrayStack表示栈
 class ArrayStack {
+    private int maxSize;// 栈的大小
+    private int[] stack;// 数组，数组模拟栈，数据就在该数组
+    private int top = -1;// top表示栈顶，初始化为-1
+
+    // 构造器
+    public ArrayStack(int maxSize){
+        this.maxSize = maxSize;
+        stack = new int[this.maxSize];
+    }
 
 }
