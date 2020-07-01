fbf87da3b7c7390f97a8b990cc8d436d7a0978aa
wangfuzheng <1677980708@qq.com>
2020-07-01 16:11:23 +0800

    编写ArrayStack2类

---
+++ src/com/quake/stack/Calculator.java
@@ -0,0 +1,51 @@
+package com.quake.stack;
+
+/**
+ * @author AKA二夕
+ * @create 2020-07-01 16:01
+ */
+public class Calculator {
+}
+
+// 定义一个ArrayStack2 表示栈
+class ArrayStack2 {
+    private int[] stack;// 数组，用于模拟栈，数据就存放在该数组
+    private int maxSize;// 栈的大小
+    private int top = -1;// 表示栈顶，初始化为-1
+
+    // 构造器
+    public ArrayStack2(int maxSize) {
+        this.maxSize = maxSize;
+        stack = new int[this.maxSize];
+    }
+
+    // 栈满
+    public boolean isFull() {
+        return top == maxSize - 1;
+    }
+
+    // 栈空
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    // 入栈 —— push
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("栈满");
+            return;
+        }
+        top++;
+        stack[top] = value;
+    }
+
+    // 出栈 —— pop
+    public int pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("栈空，没有数据~~~");
+        }
+        int value = stack[top];
+        top--;
+        return value;
+    }
+}
