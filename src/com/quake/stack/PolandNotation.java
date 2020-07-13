8ec9b6e0a06ba282cdc340ab43b4c981e611f632
wangfuzheng <1677980708@qq.com>
2020-07-13 20:55:29 +0800

    编写Operation类，返回运算符的优先级

--- src/com/quake/stack/PolandNotation.java
+++ src/com/quake/stack/PolandNotation.java
@@ -96,3 +96,34 @@ public class PolandNotation {
         return Integer.parseInt(stack.pop());
     }
 }
+
+// 编写一个类Operation
+class Operation {
+    private static int ADD = 1;
+    private static int SUB = 1;
+    private static int MUL = 2;
+    private static int DIV = 2;
+
+    // 编写一个方法，返回对应的优先级数字
+    public static int getValue(String operation) {
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = ADD;
+                break;
+            case "-":
+                result = SUB;
+                break;
+            case "*":
+                result = MUL;
+                break;
+            case "/":
+                result = DIV;
+                break;
+            default:
+                System.out.println("不存在该运算符" + operation);
+                break;
+        }
+        return result;
+    }
+}
