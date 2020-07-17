033cd362640ab6629bb5887ed9eb97cf58d95e5f
wangfuzheng <1677980708@qq.com>
2020-07-17 16:13:55 +0800

    添加寻找路线方法

--- src/com/quake/recursion/MiGong.java
+++ src/com/quake/recursion/MiGong.java
@@ -33,4 +33,36 @@ public class MiGong {
             System.out.println();
         }
     }
+
+    /**
+     * @param map 表示地图
+     * @param i   从哪个位置开始找
+     * @param j
+     * @return 如果找到通路，就返回true，否则返回false
+     */
+    public static boolean setWay(int[][] map, int i, int j) {
+        if (map[6][5] == 2) {
+            return true;
+        } else {
+            if (map[i][j] == 0) {// 如果当前这点还没有走过
+                // 按照策略 下 -> 右 -> 上 -> 左 走
+                map[i][j] = 2;// 假定该点可以走通
+                if (setWay(map, i + 1, j)) {// 向下走
+                    return true;
+                } else if (setWay(map, i, j + 1)) {// 向右走
+                    return true;
+                } else if (setWay(map, i - 1, j)) {// 向上走
+                    return true;
+                } else if (setWay(map, i, j - 1)) {// 向左走
+                    return true;
+                } else {
+                    // 说明该点是走不通，是死路
+                    map[i][j] = 3;
+                    return false;
+                }
+            } else {// 如果map[i][j] != 0 可能是1、2、3
+                return false;
+            }
+        }
+    }
 }
