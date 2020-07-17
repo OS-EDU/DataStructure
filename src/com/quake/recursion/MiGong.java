9979dba07c2c264d74c9e8a79809a2c04f2fd265
wangfuzheng <1677980708@qq.com>
2020-07-17 15:59:35 +0800

    初始化地图

---
+++ src/com/quake/recursion/MiGong.java
@@ -0,0 +1,32 @@
+package com.quake.recursion;
+
+/**
+ * @author AKA二夕
+ * @create 2020-07-17 15:53
+ */
+public class MiGong {
+    public static void main(String[] args) {
+        // 先创建一个二维数组，模拟迷宫
+        int[][] map = new int[8][7];
+        // 使用1表示墙
+        // 将上下全部置为1
+        for (int i = 0; i < 7; i++) {
+            map[0][i] = 1;
+            map[7][i] = 1;
+        }
+        // 将左右全部置为1
+        for (int i = 0; i < 8; i++) {
+            map[i][0] = 1;
+            map[i][6] = 1;
+        }
+
+        // 输出地图
+        System.out.println("地图的情况：");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
