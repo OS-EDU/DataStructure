21c647496c24ae5cd5434ace4d386af1170bcbe5
wangfuzheng <1677980708@qq.com>
2020-07-17 16:17:50 +0800

    执行递归回溯方法

--- src/com/quake/recursion/MiGong.java
+++ src/com/quake/recursion/MiGong.java
@@ -32,6 +32,18 @@ public class MiGong {
             }
             System.out.println();
         }
+
+        // 使用递归回溯给小球
+        setWay(map, 1, 1);
+
+        //输出新的地图，小球走过，并标识过的递归
+        System.out.println("小球走过，并标识过的地图情况：");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     /**
