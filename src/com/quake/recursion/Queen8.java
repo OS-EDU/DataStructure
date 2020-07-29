7a97f6b6d7c8c02d7e00a93a21eab2bafca90a7d
wangfuzheng <1677980708@qq.com>
2020-07-29 15:14:36 +0800

    八皇后问题

---
+++ src/com/quake/recursion/Queen8.java
@@ -0,0 +1,14 @@
+package com.quake.recursion;
+
+/**
+ * @author AKA二夕
+ * @create 2020-07-29 15:11
+ */
+public class Queen8 {
+    // 定义一个max表示共有多少个皇后
+    int max = 8;
+    // 定义数组array，保存皇后放置位置的结果
+    int[] array = new int[max];
+    static int count;
+    static  int judgeCount;
+}
