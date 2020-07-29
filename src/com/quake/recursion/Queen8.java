e5eacd5810a4810d2b8f98e61c27756c81e20400
wangfuzheng <1677980708@qq.com>
2020-07-29 16:24:01 +0800

    增加判断方法

--- src/com/quake/recursion/Queen8.java
+++ src/com/quake/recursion/Queen8.java
@@ -12,6 +12,27 @@ public class Queen8 {
     static int count;// 统计解法
     static int judgeCount;// 统计回溯的次数
 
+    // 查看当放置第n个皇后，检测该皇后是否和前面已经摆放的皇后冲突
+
+    /**
+     * @param n 表示第n个皇后
+     * @return 如果不冲突返回true，否则返回false
+     */
+    private boolean judge(int n) {
+        judgeCount++;
+        for (int i = 0; i < n; i++) {
+            /*
+            说明：
+                1、array[i] == array[n] 示判断第n个皇后是否和前面的n-1个皇后在同一列
+                2、Math.abs(n-i) == Math.abs(array[n] - array[i]) 表示判断第n个皇后是否和第i个皇后在同一斜线
+             */
+            if (array[i] == array[n] || Math.abs(n - i) == Math.abs(array[n] - array[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     // 输出皇后摆放的位置
     private void print() {
         count++;// 每输出一次解法，count + 1
