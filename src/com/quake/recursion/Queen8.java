aed3287a60866715816185c2622b5793dfe8f4c6
wangfuzheng <1677980708@qq.com>
2020-07-29 16:01:56 +0800

    增加输出方法

--- src/com/quake/recursion/Queen8.java
+++ src/com/quake/recursion/Queen8.java
@@ -9,6 +9,15 @@ public class Queen8 {
     int max = 8;
     // 定义数组array，保存皇后放置位置的结果
     int[] array = new int[max];
-    static int count;
-    static  int judgeCount;
+    static int count;// 统计解法
+    static int judgeCount;// 统计回溯的次数
+
+    // 输出皇后摆放的位置
+    private void print() {
+        count++;// 每输出一次解法，count + 1
+        for (int value : array) {
+            System.out.print(value + "   ");
+        }
+        System.out.println();// 换行
+    }
 }
