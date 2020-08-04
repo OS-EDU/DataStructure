aae8a6325def4df7335290c2bc19a152a7f8d99e
wangfuzheng <1677980708@qq.com>
2020-08-04 15:20:12 +0800

    测试

--- src/com/quake/sort/BubbleSort.java
+++ src/com/quake/sort/BubbleSort.java
@@ -1,10 +1,20 @@
 package com.quake.sort;
 
+import java.util.Arrays;
+
 /**
  * @author AKA二夕
  * @create 2020-08-04 15:10
  */
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] str = {3, 9, -1, 10, -2};
+        System.out.print("排序前的数组是：");
+        System.out.println(Arrays.toString(str));
+        bubbleSort(str);
+        System.out.print("排序之后的数组是：");
+        System.out.println(Arrays.toString(str));
+    }
 
 
     // 冒泡排序方法
