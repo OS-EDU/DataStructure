9f0e255c0ddf1a5de4e80b7ffc550030f2181ae4
wangfuzheng <1677980708@qq.com>
2020-08-04 15:15:35 +0800

    冒泡排序方法体

--- src/com/quake/sort/BubbleSort.java
+++ src/com/quake/sort/BubbleSort.java
@@ -6,4 +6,18 @@ package com.quake.sort;
  */
 public class BubbleSort {
 
+
+    // 冒泡排序方法
+    public static void bubbleSort(int[] arr) {
+        int temp = 0;// 临时变量
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
