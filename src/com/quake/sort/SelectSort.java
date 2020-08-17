c495c7f4db428404b3d18455127d337fcdc09405
wangfuzheng <1677980708@qq.com>
2020-08-17 11:12:01 +0800

    添加注释

--- src/com/quake/sort/SelectSort.java
+++ src/com/quake/sort/SelectSort.java
@@ -11,11 +11,12 @@ public class SelectSort {
             int minIndex = i;
             int min = arr[i];
             for (int j = i + 1; j < arr.length; j++) {
-                if (min > arr[i]) {
-                    min = arr[i];
-                    minIndex = j;
+                if (min > arr[i]) {// 说明假定的最小值不是最小
+                    min = arr[i];// 重置min
+                    minIndex = j;// 重置minIndex
                 }
             }
+            // 将最小值，放在arr[0]，即交换
             if (minIndex != i) {
                 arr[minIndex] = arr[i];
                 arr[i] = min;
