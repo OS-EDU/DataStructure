c16c217419d660e9c235cd06627a30341e49ae39
wangfuzheng <1677980708@qq.com>
2020-08-22 15:37:52 +0800

    测试希尔排序

--- src/com/quake/sort/ShellSort.java
+++ src/com/quake/sort/ShellSort.java
@@ -1,10 +1,20 @@
 package com.quake.sort;
 
+import java.util.Arrays;
+
 /**
  * @author AKA二夕
  * @create 2020-08-22 15:26
  */
 public class ShellSort {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 1, 8, 6, 5, 4, 0, 9, 7};
+        System.out.println("排序之前的数组顺序如下：");
+        System.out.println(Arrays.toString(arr));
+        shellSort01(arr);
+        System.out.println("排序之后的数组顺序如下：");
+        System.out.println(Arrays.toString(arr));
+    }
 
     // 希尔排序 使用交换法
     public static void shellSort01(int[] arr) {
