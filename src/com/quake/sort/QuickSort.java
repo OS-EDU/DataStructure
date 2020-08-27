a8907b3c98601ccd46037fc3613b8b66bbd67d86
wangfuzheng <1677980708@qq.com>
2020-08-27 21:26:26 +0800

    对pivot左右两边进行初步排序

--- src/com/quake/sort/QuickSort.java
+++ src/com/quake/sort/QuickSort.java
@@ -13,6 +13,21 @@ public class QuickSort {
         int pivot = arr[(left + right) / 2];// 中轴值
         int temp = 0;// 临时变量
 
+        // while循环的目的是让比pivot值小放到左边，比pivot值大放到右边
+        while (l < r) {
+            // 在pivot的左边一直找，找到大于等于pivot值，才退出
+            while (arr[l] < pivot) {
+                l += 1;
+            }
+            // 在pivot的右边一直找，找到小于等于pivot值，才退出
+            while (arr[r] > pivot) {
+                r -= 1;
+            }
+            // 如果 l >= r 说明pivot的左右两边的值，已经全部是小于等于pivot值，右边全部是大于等于pivot值
+            if (l >= r) {
+                break;
+            }
+        }
 
     }
 }
