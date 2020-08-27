91cb00243e4fc4d441d71c5fa527011bf95cc67d
wangfuzheng <1677980708@qq.com>
2020-08-27 21:14:58 +0800

    创建快速排序方法以及相关变量

---
+++ src/com/quake/sort/QuickSort.java
@@ -0,0 +1,18 @@
+package com.quake.sort;
+
+/**
+ * @author AKA二夕
+ * @create 2020-08-27 21:11
+ */
+public class QuickSort {
+
+    // 快速排序
+    public static void quickSort(int[] arr, int left, int right) {
+        int l = left;// 左下标
+        int r = right;// 右下标
+        int pivot = arr[(left + right) / 2];// 中轴值
+        int temp = 0;// 临时变量
+
+
+    }
+}
