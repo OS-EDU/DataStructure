91a08e47743c2aa04fe0b4f061002adad360acdc
wangfuzheng <1677980708@qq.com>
2020-08-31 15:41:16 +0800

    初始化相关变量以及说明

--- src/com/quake/sort/MergeSort.java
+++ src/com/quake/sort/MergeSort.java
@@ -19,8 +19,19 @@ public class MergeSort {
         }
     }
 
-    // 合并的方法
+    /** 合并的方法
+     *
+     * @param arr 待排序的原始数组
+     * @param left 左边有序序列的初始索引
+     * @param right 右边有序序列的初始索引
+     * @param mid 中间索引
+     * @param temp 做中转的数组
+     */
     public static void merge(int[] arr, int left, int right, int mid, int[] temp) {
+        int i = left;// 初始化i，左边有序序列的初始索引
+        int j = right;// 初始化j，右边有序序列的初始索引
+        int t = 0;// 指向temp数组的当前索引
 
+        
     }
 }
