f7a8e90221400febf66946327519667e4d6f3915
wangfuzheng <1677980708@qq.com>
2020-08-31 15:37:12 +0800

    完成 分+合 方法

--- src/com/quake/sort/MergeSort.java
+++ src/com/quake/sort/MergeSort.java
@@ -8,7 +8,15 @@ public class MergeSort {
 
     // 分+合的方法
     public static void mergeSort(int[] arr, int left, int right, int[] temp) {
-
+        if (left < right) {
+            int mid = (left + right) / 2;// 中间索引
+            // 向左递归进行分解
+            mergeSort(arr, left, mid, temp);
+            // 向右递归进行分解
+            mergeSort(arr, mid + 1, right, temp);
+            // 合并
+            merge(arr, left, right, mid, temp);
+        }
     }
 
     // 合并的方法
