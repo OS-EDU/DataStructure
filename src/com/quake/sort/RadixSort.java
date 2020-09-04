f5659412f323a6766d977dad2399596b0e5659be
wangfuzheng <1677980708@qq.com>
2020-09-04 22:28:59 +0800

    完善方法

--- src/com/quake/sort/RadixSort.java
+++ src/com/quake/sort/RadixSort.java
@@ -29,5 +29,32 @@ public class RadixSort {
 
         // 再定义一个一维数组来记录各个桶的每次放入的数据个数
         int[] bucketElementCounts = new int[10];
+
+        for (int i = 0, n = 1; i < maxLength; i++, n *= 10) {
+            // 针对各个元素的对应位进行排序处理，第一次是个位，第二次是十位，以此类推
+            for (int j = 0; j < arr.length; j++) {
+                // 取出每个元素对应位的值
+                int digitOffElement = arr[j] / n % 10;
+                // 放入对应的桶中
+                bucket[digitOffElement][bucketElementCounts[digitOffElement]] = arr[j];
+                bucketElementCounts[digitOffElement]++;
+            }
+            //按照这个桶的顺序（一维数组的下标依次取出数据，放入到原来的数组）
+            int index = 0;
+            //遍历每一个桶，并将桶中的数据放入到原数组
+            for (int k = 0; k < bucketElementCounts.length; k++) {
+                //如果桶中有数据，放入到原数组
+                if (bucketElementCounts[k] != 0) {
+                    //循环该桶即第k个桶（第k个一维数组），放入
+                    for (int l = 0; l < bucketElementCounts[k]; l++) {
+                        //取出元素放入到arr
+                        arr[index] = bucket[k][l];
+                        index++;
+                    }
+                }
+                //第i+1轮处理后，需要将每个bucketElementCounts[k] = 0
+                bucketElementCounts[k] = 0;
+            }
+        }
     }
 }
