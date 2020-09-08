c2e1879a70565efff0890a2565e3bd8c10231517
wangfuzheng <1677980708@qq.com>
2020-09-08 09:44:54 +0800

    测试线性查找

--- src/com/quake/search/SeqSearch.java
+++ src/com/quake/search/SeqSearch.java
@@ -5,6 +5,15 @@ package com.quake.search;
  * @create 2020-09-08 9:39
  */
 public class SeqSearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 9, 22, -1, 43, 293};
+        int index = seqSearch(arr, -1);
+        if (index == -1) {
+            System.out.println("没有找到对应的值");
+        } else {
+            System.out.printf("找到arr[%d] = %d", index, arr[index]);
+        }
+    }
 
     // 这里实现的线性查找是找到一个满足条件的值，就返回
     public static int seqSearch(int[] arr, int value) {
