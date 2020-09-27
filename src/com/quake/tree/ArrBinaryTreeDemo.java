c09b7b0b1406616d8e01249b4a5bbcf607eed4a9
wangfuzheng <1677980708@qq.com>
2020-09-27 22:03:53 +0800

    顺序存储二叉树遍历

---
+++ src/com/quake/tree/ArrBinaryTreeDemo.java
@@ -0,0 +1,17 @@
+package com.quake.tree;
+
+/**
+ * @author AKA二夕
+ * @create 2020-09-27 22:01
+ */
+public class ArrBinaryTreeDemo {
+}
+
+// 编写一个类，实现顺序存储二叉树遍历
+class ArrBinaryTree {
+    private int[] arr;// 纯属数据结点的数组
+
+    public ArrBinaryTree(int[] arr) {
+        this.arr = arr;
+    }
+}
