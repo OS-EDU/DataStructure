70ac146c898e70a9f4994ae6c9ac2e2dd187abad
wangfuzheng <1677980708@qq.com>
2020-10-04 14:06:10 +0800

    测试案例

--- src/com/quake/tree/threadedbinarytree/ThreadedBinaryTreeDemo.java
+++ src/com/quake/tree/threadedbinarytree/ThreadedBinaryTreeDemo.java
@@ -5,6 +5,32 @@ package com.quake.tree.threadedbinarytree;
  * @create 2020-10-04 13:36
  */
 public class ThreadedBinaryTreeDemo {
+    public static void main(String[] args) {
+        //测试一把终须线索二叉树的功能
+        HeroNode root = new HeroNode(1, "Tom");
+        HeroNode node2 = new HeroNode(3, "Jack");
+        HeroNode node3 = new HeroNode(6, "Smith");
+        HeroNode node4 = new HeroNode(8, "Mary");
+        HeroNode node5 = new HeroNode(10, "King");
+        HeroNode node6 = new HeroNode(14, "Dim");
+
+        //手动创建二叉树
+        root.setLeft(node2);
+        root.setRight(node3);
+        node2.setLeft(node4);
+        node2.setRight(node5);
+        node3.setLeft(node6);
+
+        //测试中序线索化
+        ThreadedBinaryTree threadedBinaryTree = new ThreadedBinaryTree();
+        threadedBinaryTree.setRoot(root);
+        threadedBinaryTree.threadedNodes();
+        //测试: 以10号节点测试
+        HeroNode leftNode = node5.getLeft();
+        HeroNode rightNode = node5.getRight();
+        System.out.println("10号结点的前驱结点是 =" + leftNode); //3
+        System.out.println("10号结点的后继结点是=" + rightNode); //1
+    }
 
 }
 
