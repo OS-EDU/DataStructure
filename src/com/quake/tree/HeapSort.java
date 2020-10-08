ae1cb42238bedf04f65103f7561a185c590906c3
wangfuzheng <1677980708@qq.com>
2020-10-08 20:20:24 +0800

    堆排序

--- src/com/quake/tree/HeapSort.java
+++ src/com/quake/tree/HeapSort.java
@@ -6,6 +6,26 @@ package com.quake.tree;
  */
 public class HeapSort {
 
+    // 编写一个堆排序的方法
+    public static void heapSort(int[] arr) {
+        int temp = 0;
+
+        // 1、将无序序列构建成一个堆，根据升序降序需求选择大顶堆或小顶堆
+        for (int i = arr.length / 2; i >= 0; i--) {
+            adjustHeap(arr, i, arr.length);
+        }
+
+        // 2、将堆顶元素与末尾元素交换，将最大元素“沉”到数组末端
+        // 3、重新调整结构，使其满足堆定义，然后继续交换堆顶元素与当前末尾元素
+        for (int j = arr.length - 1; j > 0; j--) {
+            // 交换
+            temp = arr[j];
+            arr[j] = arr[0];
+            arr[0] = temp;
+            adjustHeap(arr, 0, j);
+        }
+    }
+
     /**
      * 将一个数组（二叉树），调整成一个大顶堆
      * 功能：完成将以i对应的非叶子结点的树，调整成大顶堆
