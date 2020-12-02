ffb50b34761e8e8b41146590054fb4bd19aff324
wangfuzheng <1677980708@qq.com>
2020-12-02 17:02:12 +0800

    添加左旋转方法

--- src/com/quake/avl/AVLTreeDemo.java
+++ src/com/quake/avl/AVLTreeDemo.java
@@ -79,4 +79,21 @@ class Node {
         }
         return right.height();
     }
+
+    // 左旋转方法
+    private void leftRotate() {
+        // 创建新的结点，以当前根结点的值
+        Node newNode = new Node(value);
+        // 把新的结点的左子树设置成当前结点的左子树
+        newNode.left = left;
+        // 把新的结点的右子树设置成当前结点的右子树的左子树
+        newNode.right = right.left;
+        // 把当前结点的值替换成右子结点的值
+        value = right.value;
+        // 把当前面结点的右子树设置成当前结点右子树的右子树
+        right = right.right;
+        // 把当前结点的左子树（左子结点）设置成新的结点
+        left = newNode;
+
+    }
 }
