d2948c4890ffe488cfed514807d8abe94bec6a36
wangfuzheng <1677980708@qq.com>
2020-12-02 16:23:19 +0800

    初步实现添加结点的方法

--- src/com/quake/avl/AVLTreeDemo.java
+++ src/com/quake/avl/AVLTreeDemo.java
@@ -23,4 +23,28 @@ class Node {
                 "value=" + value +
                 '}';
     }
+
+    // 添加结点
+    public void add(Node node) {
+        if (node == null) {
+            return;
+        }
+        // 判断传入结点的值和当前子树根结点值的关系
+        if (node.value < this.value) {
+            // 如果当前结点的左子结点为null
+            if (this.left == null) {
+                this.left = node;
+            } else {
+                // 递归的向左子树添加
+                this.left.add(node);
+            }
+        } else {// 传入结点的值大于当前结点
+            if (this.right == null) {
+                this.right = node;
+            } else {
+                // 递归的向右子树添加
+                this.right.add(node);
+            }
+        }
+    }
 }
