7b6e5cf242119fa819c8b51292d69de55e393574
wangfuzheng <1677980708@qq.com>
2020-12-28 15:24:26 +0800

    初始化Node类结点的基本信息

--- src/com/quake/huffmantree/HuffmanTree.java
+++ src/com/quake/huffmantree/HuffmanTree.java
@@ -7,6 +7,17 @@ package com.quake.huffmantree;
 public class HuffmanTree {
 }
 
-class Node implements Comparable<Node> {
 
+// 创建类结点
+// 为了让Node对象持续排序 Collections 集合排序，实现 Comparable 接口
+class Node implements Comparable<Node> {
+    int value;// 结点权值
+    char c;// 字符
+    Node left;// 指向左子结点
+    Node right;// 指向右子结点
+
+    @Override
+    public int compareTo(Node node) {
+        return this.value - node.value;
+    }
 }
