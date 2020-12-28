7e3054aaec680a427aab74a4ff00004cd209396e
wangfuzheng <1677980708@qq.com>
2020-12-28 15:38:52 +0800

    完善 Node类中的方法

--- src/com/quake/huffmantree/HuffmanTree.java
+++ src/com/quake/huffmantree/HuffmanTree.java
@@ -1,5 +1,7 @@
 package com.quake.huffmantree;
 
+import java.time.temporal.ValueRange;
+
 /**
  * @author AKA二夕
  * @create 2020-12-28 15:16
@@ -16,8 +18,32 @@ class Node implements Comparable<Node> {
     Node left;// 指向左子结点
     Node right;// 指向右子结点
 
+    // 构造器
+    public Node(int value) {
+        this.value = value;
+    }
+
+    // 前序遍历
+    public void preOrder() {
+        System.out.println(this);
+        if (this.left != null) {
+            this.left.preOrder();
+        }
+        if (this.right != null) {
+            this.right.preOrder();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                '}';
+    }
+
     @Override
     public int compareTo(Node node) {
+        // 表示从小到大排序
         return this.value - node.value;
     }
 }
