e7ed458bc56b0abc30016af8ed2a9cbc00cf772f
wangfuzheng <1677980708@qq.com>
2021-01-19 17:53:40 +0800

    删除无效行

--- src/com/quake/huffmantree/HuffmanTree.java
+++ src/com/quake/huffmantree/HuffmanTree.java
@@ -72,7 +72,6 @@ public class HuffmanTree {
 // 为了让Node对象持续排序 Collections 集合排序，实现 Comparable 接口
 class Node implements Comparable<Node> {
     int value;// 结点权值
-    char c;// 字符
     Node left;// 指向左子结点
     Node right;// 指向右子结点
 
