374b715e471332120fbc6617603cb5cfc3b0e6a1
wangfuzheng <1677980708@qq.com>
2021-01-23 20:35:45 +0800

    创建图的方法

--- src/com/quake/graph/Graph.java
+++ src/com/quake/graph/Graph.java
@@ -1,6 +1,7 @@
 package com.quake.graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @author AKA二夕
@@ -22,4 +23,46 @@ public class Graph {
         vertexList = new ArrayList<String>(n);
         numOfEdges = 0;
     }
+
+    // 返回节点的个数
+    public int getNumOfVertex() {
+        return vertexList.size();
+    }
+
+    // 返回边的数目
+    public int getNumOfEdges() {
+        return numOfEdges;
+    }
+
+    // 显示对应的矩阵
+    public void showGraph() {
+        for (int[] edge : edges) {
+            System.out.println(Arrays.toString(edge));
+        }
+    }
+
+    // 返回 v1 和 v2 的权值
+    public int getWeight(int v1, int v2) {
+        return edges[v1][v2];
+    }
+
+    // 插入结点
+    public void insertVertex(String vertex) {
+        vertexList.add(vertex);
+    }
+
+    /**
+     * 添加边的方法
+     *
+     * @param v1     表示点的下标即第几个顶点
+     * @param v2     表示第二个顶点的下标
+     * @param weight 表示权值
+     */
+    public void insertEdge(int v1, int v2, int weight) {
+        // 保证来回路径的权值一样
+        edges[v1][v2] = weight;
+        edges[v2][v1] = weight;
+        numOfEdges++;// 边的条数 +1
+    }
+
 }
